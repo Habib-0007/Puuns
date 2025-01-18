384d905b92f21794856e0be2c90ba4968794520a
Habib-0007 <adebayohabib7@gmail.com>
2025-01-18 19:08:01 +0100

    added download functionality

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
-let generateBtn = document.querySelector(".button");
-let punNo = document.querySelector(".pun-no");
-let punText = document.querySelector(".pun-content");
+let generateBtn =
+	document.querySelector(".button");
+let punNo =
+	document.querySelector(".pun-no");
+let punText = document.querySelector(
+	".pun-content"
+);
+let punContent;
+let punId;
+generateBtn.addEventListener(
+	"click",
+	() => {
+		fetch("puns.json")
+			.then(res => res.json())
+			.then(data => {
+				let randomIndex = Math.floor(
+					Math.random() * data.length
+				);
+				punContent =
+					data[randomIndex].pun;
+				punId = data[randomIndex].id;
+				punNo.innerText = `Pun #${punId}`;
+				punText.innerText = punContent;
+			});
+	}
+);
 
-generateBtn.addEventListener("click", () => {
-    fetch("puns.json")
-    .then(res => res.json())
-    .then(data => {
-        let randomIndex = Math.floor(Math.random() * data.length);
-        let punContent = data[randomIndex].pun;
-        let punId = data[randomIndex].id;
-        punNo.innerText = `Pun #${punId}`;
-        punText.innerText = punContent;
-    })
-});
+document
+	.getElementById("downloadBtn")
+	.addEventListener("click", () => {
+		const element =
+			document.querySelector(
+				".pun-container"
+			);
+		html2canvas(element)
+			.then(canvas => {
+				const image = canvas.toDataURL(
+					"image/png"
+				);
+
+				const link =
+					document.createElement("a");
+				link.href = image;
+				link.download = `pun-${punId}.png`;
+				link.click();
+			})
+			.catch(err =>
+				alert(
+					"Error capturing element:",
+					err
+				)
+			);
+	});
